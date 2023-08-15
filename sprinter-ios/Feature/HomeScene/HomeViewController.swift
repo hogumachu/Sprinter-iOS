ee2b2c3c5bcb5bd21452cd97829cedaa0632d424
hogumachu <castle_jun@daum.net>
2023-08-16 00:05:22 +0900

    feat: add arrow button table view cell

--- sprinter-ios/Feature/HomeScene/HomeViewController.swift
+++ sprinter-ios/Feature/HomeScene/HomeViewController.swift
@@ -35,6 +35,7 @@ final class HomeViewController: BaseViewController {
         view.backgroundColor(.black)
         
         tableView.backgroundColor(.black)
+            .registerCell(ArrowTableViewCell.self)
             .registerCell(MissionTableViewCell.self)
             .setDelegate(self)
             .setDataSource(self)
@@ -53,6 +54,11 @@ extension HomeViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            
+            return tableView.dequeueCell(ArrowTableViewCell.self, for: indexPath)
+                .configure(.init(title: "60%를 달성했어요!", description: "총 6개를 완료했어요\n이번 달 남은 미션은 4개에요"))
+        }
         return tableView.dequeueCell(MissionTableViewCell.self, for: indexPath)
             .configure(mockModel[indexPath.row])
     }
