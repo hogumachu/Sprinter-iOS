53daae055797903958fe661add8e4d97b0b70398
hogumachu <castle_jun@daum.net>
2023-08-15 21:23:50 +0900

    feat: add UIFont Design System

--- sprinter-ios/Extensions/UIFont+DesignSystem.swift
+++ sprinter-ios/Extensions/UIFont+DesignSystem.swift
@@ -5,4 +5,33 @@
 //  Created by 홍성준 on 2023/08/15.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    
+    // MARK: - Small
+    
+    static let smallR = UIFont.systemFont(ofSize: 13, weight: .regular)
+    static let smallSB = UIFont.systemFont(ofSize: 13, weight: .semibold)
+    static let smallB = UIFont.systemFont(ofSize: 13, weight: .bold)
+    
+    
+    // MARK: - Medium
+    
+    static let mediumR = UIFont.systemFont(ofSize: 17, weight: .regular)
+    static let mediumSB = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    static let mediumB = UIFont.systemFont(ofSize: 17, weight: .bold)
+    
+    // MARK: - Large
+    
+    static let largeR = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let largeSB = UIFont.systemFont(ofSize: 20, weight: .semibold)
+    static let largeB = UIFont.systemFont(ofSize: 20, weight: .bold)
+    
+    // MARK: - ExtraLarge
+    
+    static let extraLargeR = UIFont.systemFont(ofSize: 25, weight: .regular)
+    static let extraLargeSB = UIFont.systemFont(ofSize: 25, weight: .semibold)
+    static let extraLargeB = UIFont.systemFont(ofSize: 25, weight: .bold)
+    
+}
