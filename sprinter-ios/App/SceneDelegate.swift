e582223cf19de178046d72621e3cd01223a087bb
hogumachu <castle_jun@daum.net>
2023-08-15 22:01:22 +0900

    feat: add home scene

--- sprinter-ios/App/SceneDelegate.swift
+++ sprinter-ios/App/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: scene)
         self.window = window
+        window.rootViewController = HomeViewController()
         window.makeKeyAndVisible()
     }
 
---
+++ sprinter-ios/Scene/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  sprinter-ios
+//
+//  Created by 홍성준 on 2023/08/15.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .mint1
+    }
+    
+}
