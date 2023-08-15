dcb67e514aafa86bede2160d22001f5553faaf0c
hogumachu <castle_jun@daum.net>
2023-08-15 22:55:24 +0900

    feat: update home scene ui

--- sprinter-ios/Feature/HomeScene/HomeViewController.swift
+++ sprinter-ios/Feature/HomeScene/HomeViewController.swift
@@ -6,12 +6,55 @@
 //
 
 import UIKit
+import SnapKit
 
-final class HomeViewController: UIViewController {
+final class HomeViewController: BaseViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .mint1
+    // TODO: - Remove Mock Data
+    private let mockModel: [MissionTableViewCellModel] = [
+        .init(mission: "A"),
+        .init(mission: "B"),
+        .init(mission: "C"),
+        .init(mission: "D"),
+        .init(mission: "E"),
+        .init(mission: "F"),
+        .init(mission: "G"),
+        .init(mission: "H"),
+    ]
+    
+    private let tableView = UITableView(frame: .zero)
+    
+    override func setupLayout() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    override func setupAttributes() {
+        view.backgroundColor(.black)
+        
+        tableView.backgroundColor(.black)
+            .registerCell(MissionTableViewCell.self)
+            .setDelegate(self)
+            .setDataSource(self)
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        mockModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueCell(MissionTableViewCell.self, for: indexPath)
+            .configure(mockModel[indexPath.row])
     }
     
 }
