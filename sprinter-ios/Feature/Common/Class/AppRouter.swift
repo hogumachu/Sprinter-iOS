573b89074dec7a5eef392e13273a3517679d83ee
hogumachu <castle_jun@daum.net>
2023-08-16 22:01:55 +0900

    feat: add router, scene factory

--- sprinter-ios/Feature/Common/Class/AppRouter.swift
+++ sprinter-ios/Feature/Common/Class/AppRouter.swift
@@ -5,4 +5,73 @@
 //  Created by 홍성준 on 2023/08/16.
 //
 
-import Foundation
+import UIKit
+
+enum TransitionStyle {
+    case modal
+    case push
+}
+
+enum CloseStyle {
+    case pop
+    case dismiss
+}
+
+protocol AppRouterType {
+    func start(root: Scene)
+    func transition(to scene: Scene, using style: TransitionStyle, animated: Bool, completion: (() -> Void)?)
+    func close(using style: CloseStyle, animated: Bool, completion: (() -> Void)?)
+}
+
+final class AppRouter: AppRouterType {
+    
+    private var currentNavigationController: UINavigationController?
+    private let factory: SceneFactoryType
+    
+    init(factory: some SceneFactoryType) {
+        self.factory = factory
+    }
+    
+    func start(root: Scene) {
+        let rootViewController = factory.create(scene: .root)
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.setNavigationBarHidden(true, animated: false)
+        navigationController.interactivePopGestureRecognizer?.delegate = nil
+        self.currentNavigationController = navigationController
+        UIApplication.keyWindow?.rootViewController = navigationController
+    }
+    
+    func transition(to scene: Scene, using style: TransitionStyle, animated: Bool, completion: (() -> Void)?) {
+        guard let topViewController = UIViewController.topViewController else { return }
+        currentNavigationController = topViewController as? UINavigationController ?? topViewController.navigationController
+        let viewController = factory.create(scene: scene)
+        
+        switch style {
+        case .modal:
+            let navigationController = UINavigationController(rootViewController: viewController)
+            navigationController.setNavigationBarHidden(true, animated: false)
+            navigationController.interactivePopGestureRecognizer?.delegate = nil
+            navigationController.modalPresentationStyle = .overFullScreen
+            currentNavigationController = navigationController
+            topViewController.present(navigationController, animated: animated, completion: completion)
+            
+        case .push:
+            currentNavigationController?.pushViewController(viewController, animated: animated)
+        }
+    }
+    
+    func close(using style: CloseStyle, animated: Bool, completion: (() -> Void)?) {
+        switch style {
+        case .pop:
+            currentNavigationController?.popViewController(animated: animated)
+            
+        case .dismiss:
+            currentNavigationController?.dismiss(animated: animated, completion: { [weak self] in
+                completion?()
+                let topViewController = UIViewController.topViewController!
+                self?.currentNavigationController = topViewController as? UINavigationController ?? topViewController.navigationController
+            })
+        }
+    }
+    
+}
--- sprinter-ios/Feature/Common/Class/SceneFactory.swift
+++ sprinter-ios/Feature/Common/Class/SceneFactory.swift
@@ -5,4 +5,33 @@
 //  Created by 홍성준 on 2023/08/16.
 //
 
-import Foundation
+import UIKit
+
+enum Scene {
+    case root
+    case missionCreate
+}
+
+protocol SceneFactoryType {
+    func create(scene: Scene) -> UIViewController
+}
+
+final class SceneFactory: SceneFactoryType {
+    
+    func create(scene: Scene) -> UIViewController {
+        let router = AppRouter(factory: self)
+        
+        switch scene {
+        case .root:
+            let reactor = RootReactor()
+            let viewController = RootViewController(reactor: reactor)
+            return viewController
+            
+        case .missionCreate:
+            let reactor = MissionCreateReactor()
+            let viewController = MissionCreateViewController(reactor: reactor)
+            return viewController
+        }
+    }
+    
+}
